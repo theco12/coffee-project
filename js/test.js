14ffe44d64345414b0b61d0300024f1d84ef9162
theco12 <107103821+theco12@users.noreply.github.com>
2022-10-25 16:37:26 +0900

    update

--- js/test.js
+++ js/test.js
@@ -1,16 +1,9 @@
-let a = 100; //전역변수
+let popup = document.getElementById("popup");
 
-function sample() {
-  let a = "내부의 A";
-  let b = 200; //함수안에서 만든 함수를 지역변수
-  console.log(a);
+function openPopup() {
+  popup.classList.add("open-popup");
 }
 
-sample();
-// console.log(b);
-
-let 가격 = 10000;
-const 부가가치세율 = 0.1;
-let 부가가치세 = 가격 * 부가가치세율;
-
-console.log(부가가치세);
+function ClosePopup() {
+  popup.classList.remove("open-popup");
+}
