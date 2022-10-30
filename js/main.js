2804fa2140cb891809df0728adb1697ecd242d9d
theco12 <107103821+theco12@users.noreply.github.com>
2022-10-30 19:48:17 +0900

    update

--- js/main.js
+++ js/main.js
@@ -45,3 +45,11 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1,
   });
 });
+
+// swiper
+
+new Swiper(".swiper-container", {
+  direction: "vertical", // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+});
