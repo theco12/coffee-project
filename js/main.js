2290f8cc021b1767c8d2c179d5b06210d1989ff0
theco12 <107103821+theco12@users.noreply.github.com>
2023-08-14 14:34:11 +0900

    update eventloop

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ window.addEventListener(
         display: "block",
       });
     }
-  }, 300)
+  }, 300),
 );
 // _.throttle(함수, 시간)
 
@@ -48,8 +48,29 @@ fadeEls.forEach(function (fadeEl, index) {
 
 // swiper
 
-new Swiper(".swiper-container", {
+new Swiper(".notice-line .swiper-container", {
   direction: "vertical", // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true, // 반복 재생 여부
+});
+
+new Swiper(".promotion .swiper-container", {
+  direction: "horizontal", // 수평 슬라이드
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
